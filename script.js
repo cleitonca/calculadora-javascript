e787c25e6688c9c7c4416fcc326923c500ecf77a
Cleiton Araújo <cltn.info@gmail.com>
2020-11-17 21:17:41 -0300

    Funções

--- script.js
+++ script.js
@@ -1,19 +1,58 @@
 
 var displayBuffer = "";
 var numero = "";
+var termos = [undefined,undefined,undefined];
 
 function pressNum(num) {
     numero = numero.concat(num.innerHTML);
     showDisplay(num.innerHTML);
 }
 
-function clearMemory() {
-    numero = "";
+function pressOperation(op){
+    termos[0]=numero;
+    termos[1]=op.innerHTML;
+    showDisplay(op.innerHTML);
+    numero="";
+}
+
+function pressEquals(){
+    termos[2] = numero;
+    switch(termos[1]){
+        case '+': 
+            resultado = Number(termos[0])+Number(termos[2]);
+            break;
+        case '-': 
+            resultado = Number(termos[0])-Number(termos[2]);
+            break;
+        case '*': 
+            resultado = Number(termos[0])*Number(termos[2]);
+            break;
+        case '/': 
+            resultado = Number(termos[0])/Number(termos[2]);
+            break;
+    }
+    clearDisplay();
+    showDisplay(resultado);
 }
 
 function showDisplay(conteudo){
     displayBuffer = displayBuffer.concat(conteudo);
     var tela = document.getElementById('display')
     tela.value = displayBuffer;
-    
 }
+
+function clearMemory() {
+    numero = "";
+}
+
+function clearDisplay(){
+    displayBuffer = "";
+    var tela = document.getElementById('display');
+    tela.value = displayBuffer;
+}
+
+function clearAll(){
+    clearDisplay();
+    clearMemory();
+}
+
